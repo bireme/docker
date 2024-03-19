be0fb961589e899d6ab51646a2b5d9abd632d65c
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-03-19 10:58:56 -0300

    [Dremio] Arquivos iniciais de configuração para v 24.3

---
+++ dremio/executor/start.sh
@@ -0,0 +1,7 @@
+# https://hub.docker.com/r/dremio/dremio-oss/
+# https://docs.dremio.com/current/get-started/cluster-deployments/deployment-models/standalone/
+
+# update version
+docker pull dremio/dremio-oss:24.3
+
+docker compose up
